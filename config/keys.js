dc4eea7f4e49995ce0ba9fb3f16fe34ba45b67b9
Raveendra Reddy Vennapusa <raveendra.vennapusa.contractor@move.com>
2020-01-31 09:45:39 +0530

    make it bette

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
-    module.exports = require('./prod');
+    module.exports = require('./dev');
   } else {
     // we are in development - return the dev keys!!!
     module.exports = require('./dev');
